ff562335202d3d610f43a1f0692d3e6d69d0fcbd
chenshinshi <125173405+chenshinshi@users.noreply.github.com>
2023-09-02 18:17:32 +0800

    1.1.0
    新增显示文档图标

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 为没有装配链接图标的主题（如 cliff-dark, toy）提供链接图标。
 
 感谢[Achuan-2 Tsundoku 主题](https://github.com/Achuan-2/siyuan-themes-tsundoku)提供的 148 种图标。
+
+支持引用页面时显示页面图标，感谢[mdzz2048 提供的代码](https://github.com/chenshinshi/link-icon/issues/1)。
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-"use strict";const s=require("siyuan");class e extends s.Plugin{}module.exports=e;
+"use strict";
+const siyuan = require("siyuan");
+
+async function getDocIcon(block_id) {
+    let response = await siyuan.fetchSyncPost(
+        '/api/block/getDocInfo', 
+        {
+            id: block_id
+        }
+    )
+    let icon_code = response.code === 200 ? null : response.data.icon
+    // 这里可以考虑参考目录插件，插入文档默认的图标
+    let block_icon = icon_code 
+        ? String.fromCodePoint(parseInt(icon_code, 16))
+        : ""
+    return block_icon;
+}
+
+
+class LinkIconPlugin extends siyuan.Plugin{
+    async onload() {
+        this.eventBus.on('loaded-protyle', this.listeners)
+    }
+
+    async unload() {
+        this.eventBus.off('loaded-protyle', this.listeners)
+    }
+
+    async listeners(event) {
+        // 仅给触发加载文档的元素添加块引用图标
+        let doc = event.detail.element;
+        let ref_list = doc.querySelectorAll("span[data-type='block-ref']")
+
+        for (let index = 0; index < ref_list.length; index++) {
+            let element = ref_list[index];
+            let block_id = element.attributes["data-id"].value;
+            let block_icon = await getDocIcon(block_id);
+            let html = element.innerHTML;
+            element.innerHTML = html.startsWith(block_icon) ? html : block_icon + html;
+        }
+    }
+}
+
+module.exports = LinkIconPlugin;
