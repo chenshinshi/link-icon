45761c0d0a2b7b81c7898037864d9de18bffb1d5
frostime <frostime@foxmail.com>
2023-09-03 21:29:07 +0800

    refactor: 使用 insert dom 的方式插入图标

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 const siyuan = require("siyuan");
 
+const ICON_CLASS = "plugin-link-icon";
+
 async function request(url, data) {
     // info(`Request: ${url}; data = ${JSON.stringify(data)}`);
     let response = await siyuan.fetchSyncPost(url, data);
@@ -18,7 +20,16 @@ async function sql(sql) {
     return request(url, sqldata);
 }
 
-async function getDocIcon(block_id) {
+/**
+ * 获取文档块的图标
+ * @param {string} block_id
+ * @returns icon_dom
+ *    - null: 不是文档块
+ *    - <img class="plugin-link-icon" />: svg 图标
+ *    - <span class="plugin-link-icon" />: emoji 图标
+ */
+async function getDocIconDom(block_id) {
+    //如果不是文档块，则不添加图标
     let blocks = await sql(`select * from blocks where id = "${block_id}"`);
     if (blocks?.[0] === null || blocks[0].type !== 'd') {
         // console.log(`block ${block_id} is not a doc`)
@@ -35,22 +46,25 @@ async function getDocIcon(block_id) {
         return null;
     }
 
-    //@string
     let icon_code = response.data.icon;
     let sub_file_cnt = response.data.subFileCount;
+    // 默认文档图标
     if (icon_code === "") {
-        return sub_file_cnt > 0 ? "📑" : "📄";
+        return sub_file_cnt > 0
+            ? `<span class="${ICON_CLASS}">📑</span>`
+            : `<span class="${ICON_CLASS}">📄</span>`;
     }
 
-    let icon = "";
+    let icon_dom = "";
+    //使用了自定义的 svg 图标 vs 使用 unicode 编码的 emoji
     if (icon_code.toLowerCase().endsWith(".svg")) {
-        // icon = `<img alt="${icon_code} class="emoji" src="/emojis/${icon_code}" title="${icon_code}">`
-        icon = ""
+        icon_dom = `<img alt="${icon_code}" class="emoji ${ICON_CLASS}" src="/emojis/${icon_code}" title="${icon_code}">`
     } else {
-        icon = String.fromCodePoint(parseInt(icon_code, 16))
+        icon_dom = String.fromCodePoint(parseInt(icon_code, 16))
+        icon_dom = `<span class="${ICON_CLASS}">${icon_dom}</span>`
     }
 
-    return icon;
+    return icon_dom;
 }
 
 
@@ -70,13 +84,19 @@ class LinkIconPlugin extends siyuan.Plugin{
 
         for (let index = 0; index < ref_list.length; index++) {
             let element = ref_list[index];
+
+            // 如果前一个元素是图标，则不再添加
+            let previes_sibling = element.previousSibling;
+            if (previes_sibling !== null && previes_sibling.classList.contains(ICON_CLASS)) {
+                continue;
+            }
+
             let block_id = element.attributes["data-id"].value;
-            let block_icon = await getDocIcon(block_id);
+            let block_icon = await getDocIconDom(block_id);
             if (block_icon === null) {
                 continue;
             }
-            let html = element.innerHTML;
-            element.innerHTML = html.startsWith(block_icon) ? html : block_icon + html;
+            element.insertAdjacentHTML('beforebegin', block_icon);
         }
     }
 }
